6755f5498e9f7b6fbcc9a56149dee19c088ae0da
olga <olgaegorova11@gmail.com>
2021-08-03 13:58:09 +0100

    Updating the documentation for "candidate_set_orth"

--- R/candidate_set_orth.R
+++ R/candidate_set_orth.R
@@ -1,11 +1,11 @@
 #' Forms the candidate set
 #'
-#' This function forms the full extended candidate set, primary terms, with labels, orthonormalised.
-#' @param cand Candidate set.
-#' @return The candidate set for potential terms, with labels, orthogonalised.
+#' This function forms the full extended orthonormalised candidate set, with labels.
+#' @param cand Candidate set containing primary terms, with labels in the first column.
+#' @return The orthonormalised full candidate set containing primary and potential terms, with labels.
 #' @export
 #' @examples
-#' candidate_set(rep(list(1:3),5)) # Candidate set of 5 factors each of 3 levels
+#' candidate_set_orth(candidate_set(rep(list(1:3),5))) # Candidate set of 5 factors each of 3 levels
 
 candidate_set_orth<-function(cand)
 {
