2204b435d686481bf855f3e31fcc1a847c4ba645
olga <olgaegorova11@gmail.com>
2021-08-11 14:54:05 +0100

    Creating documentation and examples for MSE-based criteria

--- R/criteria_mseD.R
+++ R/criteria_mseD.R
@@ -1,6 +1,35 @@
+#' Calculates the values of the MSE DPs-criterion and its components
+#'
+#' This function evaluates the MSE DPs-criterion for given primary and potential model matrices. Candidate full model matrices do not have to be orthonormalised.
+#' Components: DPs-, LoF(DP)- and MSE(D)-optimality.
+#' The weights kappa.DP, kappa.LoF and kappa.mse, and other parameters (tau2, alpha-s) are taken from the global environment.
+#'
+#' @param X1 The primary model matrix, with the first column containing the labels of treatments, and the second -- the intercept term.
+#' @param X2 The matrix of potential terms, with the first column containing the labels of treatments.
+#' @param eps Computational tolerance, the default value is 10^-23
+#'
+#' @return A list of values: indicator of whether the evaluation was successful ("eval"), DPs-criterion value -- intercept excluded ("DP"),
+#' Lack-of-fit(DP) criterion value ("LoF"), the MSE(D) component value ("mse"), 
+#' the number of pure error degrees of freedom ("df") and the value of the compound criterion ("compound").
+#' @export
+#' @examples 
+#'# Experiment: one 5-level factor, primary model -- full quadratic, X^3 and X^4 potential terms.
+#' K<-1; P<-3; Q<-2;
+#'Levels <- list(1:5)   
+#'# Generating candidate sets: primary and full ones
+#'cand.primary <- candidate_set(Levels);
+#'cand.full <- cbind(cand.primary, cand.primary[,3]^3, cand.primary[,4]^2) #X^3 and X^4 potential terms
+#'# Choosing a design
+#'index <- c(rep(1,2),3,rep(4,2),rep(5,3)); Nruns<- length(index)
+#'Z0<-diag(1,Nruns)-matrix(1/Nruns,nrow=Nruns,ncol=Nruns)
+#'X.primary <- cand.full[index, 1:(P+1)]
+#'X.potential <- cand.full[index, (c(1,(P+2):(P+Q+1)))]
+#'# Evaluating a compound MSE(DP)-criterion
+#'kappa.DP = kappa.LoF = kappa.mse = 1./3; tau2 <-1; tau <- sqrt(tau2)
+#'alpha.DP = alpha.LoF = 0.05; Biter <- 1000; 
+#'criteria.mseD(X1 = X.primary, X2 = X.potential)
+#' # Output: eval = 1, DP = 2.682, LoF = 6.455, mse = .8854, df = 4, compound = 2.4842
 
-### Function used to find unblocked MSE(D)-optimal designs (nearly optimal)
-### Minimising. Components: DPs, LoF(P), mse(D)
 criteria.mseD<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary and potential terms, both with labels
 {
   Ds<-0; DP<-0; LoF<-0; bias<-0; mse<-0;
@@ -8,24 +37,24 @@ criteria.mseD<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
   df<-Nruns-DF                                # df - pure error degrees of freedom
 
   M<-crossprod(X1[,-1])                       # information matrix of primary terms
-  D<-prod(round(eigen(M,symmetric=TRUE,only.values=TRUE)$values,8)) #/Nruns
+  D<-prod(round(eigen(M,symmetric=TRUE,only.values=TRUE)$values,8)) 
   if (D>eps)
   {
     Minv<-solve(M)
-  } else {return (list (eval=0, Ds=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if M is computationally singular
-  if (kappa.Ds>0 || (kappa.DP>0))  ###Check this bit
+  } else {return (list (eval=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if M is computationally singular
+  if (kappa.Ds>0 || (kappa.DP>0))  
   {
     if (D^(1.0/(P-1))>0)
     {
       Ds<-(D/Nruns)^(-1.0/(P-1))
-    } else {return (list (eval=0, Ds=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # Ds
+    } else {return (list (eval=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # Ds
   }
   if (kappa.DP>0)
   {
     if (df>0)
     {
       DP<-Ds*qf(1-alpha.DP,P-1,df)             # DPs
-    } else {return (list (eval=0, Ds=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if df=0
+    } else {return (list (eval=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if df=0
   }
   if (((kappa.LoF>0) && (df>0))||(kappa.mse>0))  # check for A calculation
   {
@@ -38,7 +67,7 @@ criteria.mseD<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
     {
       L0<-crossprod(X2[,-1])-t(M12)%*%A+diag(1./tau2,nrow=Q)
       LoF<-(det(L0))^(-1.0/Q)*qf(1-alpha.LoF,Q,df) #Ds
-    } else {return (list (eval=0, Ds=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if df=0
+    } else {return (list (eval=0, DP=0, LoF=0, bias=0, df=df, compound=10^6));} # if df=0
   }
   if (kappa.mse>0)
   {
@@ -55,5 +84,5 @@ criteria.mseD<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
     mse<-(exp(MC)*Nruns/D)^(1./(P-1))                                 # MSE(Ds)
   }
   compound<-DP^kappa.DP*LoF^kappa.LoF*mse^kappa.mse
-  list (eval=1,DP=DP, LoF=LoF, mse=mse, df=df, compound=compound)
+  list (eval=1, DP=DP, LoF=LoF, mse=mse, df=df, compound=compound)
 }
--- R/criteria_mseL.R
+++ R/criteria_mseL.R
@@ -1,10 +1,39 @@
-
-### Function used to find unblocked MSE(L)-optimal designs (nearly optimal)
-### Minimising Components: LP, LoF(LP), mse(L)
+#' Calculates the values of the MSE LPs-criterion and its components
+#'
+#' This function evaluates the MSE LPs-criterion for given primary and potential model matrices. Candidate full model matrices do not have to be orthonormalised.
+#' Components: LPs-, LoF(LP)- and MSE(L)-optimality.
+#' The weights kappa.LP, kappa.LoF and kappa.mse, and other parameters (tau2, alpha-s) are taken from the global environment.
+#'
+#' @param X1 The primary model matrix, with the first column containing the labels of treatments, and the second -- the intercept term.
+#' @param X2 The matrix of potential terms, with the first column containing the labels of treatments.
+#' @param eps Computational tolerance, the default value is 10^-23
+#'
+#' @return A list of values: indicator of whether the evaluation was successful ("eval"), LPs-criterion value -- intercept excluded ("LP"),
+#' Lack-of-fit(LP) criterion value ("LoF"), the MSE(L) component value ("mse"), 
+#' the number of pure error degrees of freedom ("df") and the value of the compound criterion ("compound").
+#' @export
+#' @examples 
+#'# Experiment: one 5-level factor, primary model -- full quadratic, X^3 and X^4 potential terms.
+#' K<-1; P<-3; Q<-2;
+#'Levels <- list(1:5)   
+#'# Generating candidate sets: primary and full ones
+#'cand.primary <- candidate_set(Levels);
+#'cand.full <- cbind(cand.primary, cand.primary[,3]^3, cand.primary[,4]^2) #X^3 and X^4 potential terms
+#'# Choosing a design
+#'index <- c(rep(1,2),3,rep(4,2),rep(5,3)); Nruns<- length(index)
+#'Z0<-diag(1,Nruns)-matrix(1/Nruns,nrow=Nruns,ncol=Nruns)
+#'X.primary <- cand.full[index, 1:(P+1)]
+#'X.potential <- cand.full[index, (c(1,(P+2):(P+Q+1)))]
+#'# Evaluating a compound MSE(LP)-criterion
+#'kappa.LP = kappa.LoF = kappa.mse = 1./3; tau2 <-1; tau <- sqrt(tau2)
+#'alpha.LP = alpha.LoFL = 0.05;  
+#'criteria.mseL(X1 = X.primary, X2 = X.potential)
+#' # Output: eval = 1, LP = 2.3863, LoF = 7.1846, mse = 1.5994, df = 4, compound = 2.8047
+#' 
 criteria.mseL<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary and potential terms, both with labels
 {
   Ls<-0; LP<-0; LoF<-0; mse<-0;
-  DF<-nlevels(as.factor(X1[,1]))              #
+  DF<-nlevels(as.factor(X1[,1]))              
   df<-Nruns-DF                                # df - pure error degrees of freedom
 
   M<-crossprod(X1[,-1])                       # information matrix of primary terms
@@ -12,7 +41,7 @@ criteria.mseL<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
   if (D>eps)
   {
     Minv<-solve(M)
-  } else {return (list (Ls=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
+  } else {return (list (eval=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
 
   if ((kappa.Ls>0)||(kappa.LP>0))
   {
@@ -23,9 +52,9 @@ criteria.mseL<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
     if (df>0)
     {
       LP<-Ls*qf(1-alpha.LP,1,df)              # LPs
-    } else {return (list (Ls=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
+    } else {return (list (eval=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
   }
-  if (((kappa.LoF>0) && (df>0))||(kappa.mse>0))                        # check for A calculation
+  if (((kappa.LoF>0) && (df>0))||(kappa.mse>0))         # check for A calculation
   {
     M12<-crossprod(X1[,-1],X2[,-1])
     A<-Minv%*%M12
@@ -37,7 +66,7 @@ criteria.mseL<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
       L0<-crossprod(X2[,-1])-t(M12)%*%A+diag(1./tau2,nrow=Q)            # dispersion matrix + Iq/tau2
       L0.inv.trace<-Re(sum(1./eigen(L0,only.values=TRUE)$values))       # trace of the inverse matrix
       LoF<-L0.inv.trace*qf(1-alpha.LoFL,1,df)/Q
-    } else {return (list (Ls=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
+    } else {return (list (eval=0, LP=0, LoF=0, bias=0, df=df, compound=10^6));}
   }
   if (kappa.mse>0)
   {
@@ -45,5 +74,5 @@ criteria.mseL<-function(X1,X2,eps=10^-23)      # X1, X2 -- matrices of primary a
     mse<-sum(diag(A0)[-1])/(P-1)                                        # averaged trace of the A'A+Iq
   }
   compound<-LP^kappa.LP*LoF^kappa.LoF*mse^kappa.mse
-  list (Ls=Ls, LP=LP, LoF=LoF, mse=mse, df=df, compound=compound)
+  list (eval=1, LP=LP, LoF=LoF, mse=mse, df=df, compound=compound)
 }
