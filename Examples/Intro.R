ea12c640601e31ac1d509f6cb1b98de36b9824fd
vkstats <val.koutra@gmail.com>
2022-12-09 14:58:47 +0000

    Update Intro.R

--- Examples/Intro.R
+++ Examples/Intro.R
@@ -1,4 +1,4 @@
-example<-settings(K=3,Klev=3,Nruns=40)
-print.settings(example) #info
-mood<-Search() #obtain mood for the given settings
+mood.example <- settings(K = 3, Klev = 3, Nruns = 40)
+print.settings(mood.example) #info
+mood <- Search(mood.example) #obtain mood for the given settings
 mood
