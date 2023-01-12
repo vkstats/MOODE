508ee09e42abbebc95edd656d8968b2fd245f09a
olga <olgaegorova11@gmail.com>
2023-01-12 15:38:23 +0000

    Adding column names to the candidate matrix of polynomial terms
    Changing the "candidate_set_full" function to produce the candidate points with all the polynomial terms up to and including the third order. Might be good to extend to the order of 4.

--- R/candidate_set_full.R
+++ R/candidate_set_full.R
@@ -1,34 +1,25 @@
-#' Forms the full candidate set
+#' Forms the full candidate set of treatments and polynomial terms
 #'
-#' This function forms the full extended candidate set with all possible primary and potential terms, with labels, not orthonormalised.
+#' This function forms the full extended candidate set with all polynomial terms, with labels, not orthonormalised.
 #' @param cand Candidate set of treatments, the first column contains treatment labels. 
 #' Usually obtained as output from the "candidate_set" function.
-#' @return The full extended candidate set: the column of treatment labels, then named columns with polynomial terms up to the order of 4. 
-#' For example, "x12" stands for $x_1^2$, and "x1x2 stands for $x_1x_2$, and "x23x4" -- for $x_2^3x_4$
+#' @return The full extended candidate set: the column of treatment labels, then named columns with polynomial terms up to the order of 3. 
+#' For example, "x12" stands for $x_1^2$, and "x1x2 stands for $x_1x_2$, and "x23x4" -- for $x_2^3x_4$.
 #' @export
 #' @examples
 #' 
-#' # Full extended candidate set for two 3-level factors, full quadratic polynomial model
+#' # Full extended candidate set for two 3-level factors
 #' 
 #' K<-2; Levels <- rep(list(1:3),K);
-#' Parameters <- c(1, rep(1,K), rep(1,K), K*(K-1)/2)
-#' candidate_set_full(candidate_set(Levels))
-
-candidate_set_full<-function(cand, K)
-{
-  cand.full<-cbind(cand, potential.matrix(cand, K)[,-1])
-  return (cand.full)
-}
-
-##### Creating a full candidate model matrix, from a candidate matrix of treatments (ie linear terms)
+#' candidate_set_full(candidate_set(Levels, K), K)
 
 candidate_set_full = function(cand, K) {
-  K = ncol(cand.m)       # number of factors
-  cand.terms = cand[,-1]
   
-  ### Linear and quadratic terms
+  cand.terms = cand[,-1]  #  linear terms
+  
+  ### Naming linear and generating and naming quadratic terms
   for (k in 1:K) {
-    colnames(cand.terms)[k] = paste("x", as.character(k), sep = "") # linear terms, eg "x3"
+    colnames(cand.terms)[k] = paste("x", as.character(k), sep = "")  # named linear terms, e.g. "x3"
     cand.terms = cbind(cand.terms, cand.terms[,k]^2)
     colnames(cand.terms)[K+k] = paste("x", as.character(k), as.character(2), sep = "") # quadratic terms: "x12", "x22", "x32", etc.
   }
@@ -38,22 +29,23 @@ candidate_set_full = function(cand, K) {
     int.count = 0
     for (k in 1:(K-1)){
       for (j in (k+1):K) {
-        int.count = int.count + 1                                       # count the number of interaction terms
+        int.count = int.count + 1                  # count the number of interaction terms
         cand.terms = cbind(cand.terms, cand.terms[,k]* cand.terms[,j])
         colnames(cand.terms)[2*K + int.count] = 
           paste("x", as.character(k), "x", as.character(j), sep = "")   # interaction terms: "x1x2", "x2x3", "x3x4", etc.
       }
     }
     
-    ### Interaction terms: quadratic by linear
+    ### Interaction terms: quadratic by linear 
     int2.count = 0
     nterms = ncol(cand.terms)
     for (k in 1:(K-1)){
       for (j in (k+1):K) {
-        int2.count = int2.count + 1                                 # count the number of QxL interaction terms
-        cand.terms = cbind(cand.terms, cand.terms[,k]^2 * cand.terms[,j])
-        colnames(cand.terms)[nterms + int2.count] = 
-          paste("x", as.character(k), as.character(2), "x", as.character(j), sep = "")   # QxL interaction terms: "x12x2", "x22x3", "x32x4", etc.
+        int2.count = int2.count + 2                # count the number of QxL interaction terms
+        cand.terms = cbind(cand.terms, cand.terms[,k]^2 * cand.terms[,j], cand.terms[,k] * cand.terms[,j]^2)
+        colnames(cand.terms)[nterms + int2.count - c(1,0)] = 
+          c(paste("x", as.character(k), as.character(2), "x", as.character(j), sep = ""),
+            paste("x", as.character(k), "x", as.character(j), as.character(2), sep = "")) # QxL  and LxQ interaction terms: "x12x2", "x1x22", etc.
       }
     }
   }
@@ -65,7 +57,7 @@ candidate_set_full = function(cand, K) {
     for (k in 1:(K-2)){
       for (j in (k+1):(K-1)) {
         for (i in (j+1):K){
-          int3.count = int3.count + 1                                 # count the number of LxLxL interaction terms
+          int3.count = int3.count + 1                     # count the number of LxLxL interaction terms
           cand.terms = cbind(cand.terms, cand.terms[,k] * cand.terms[,j]* cand.terms[,i])
           colnames(cand.terms)[nterms + int3.count] = 
             paste("x", as.character(k), "x", as.character(j), "x", as.character(i), sep = "")   # LxLxL terms: "x1x2x3", "x1x3x4", etc.
@@ -81,6 +73,8 @@ candidate_set_full = function(cand, K) {
     cand.terms = cbind(cand.terms, cand.terms[,k]^3)
     colnames(cand.terms)[nterms + k] = paste("x", as.character(k) , as.character(3), sep = "")   # cubic terms: "x13", "x33", etc.
   }
+  
+  cand.terms = cbind(cand[,1], cand.terms)    # append the column of labels
   
   return (cand.terms)
 }
