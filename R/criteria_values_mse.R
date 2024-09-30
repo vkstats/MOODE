460e657a4aa6fb3ac3215eace3a7bc29d8b272e7
olga <olgaegorova11@gmail.com>
2024-09-30 12:03:07 +0100

    Adding `criteria_values_mse' function evaluating all component criteria, post-search

--- R/criteria_values_mse.R
+++ R/criteria_values_mse.R
@@ -1,15 +1,45 @@
+#' Evaluating individual criteria of the designs
+#' @description Calculating values of determinant- and trace-based components of 
+#' MSE(D)- and MSE(L)- criteria for an output of a search object, 
+#' with model and control parameters set in a mood object.
+#' 
+#' @param search.obj Output of the `Search' function
+#' @param mood.obj Output of the `mood' function
+#' @param eps Computational tolerance, default 10^-20
+#' @param Biter MC sample size for evaluating the mse(D)-component
+#' 
+#' @return List of the calculated values:
+#' \itemize{
+#' \item `df` pure error degrees of freedom
+#' \item `Ds` Ds-criterion value, intercept excluded
+#' \item `DP` DPs-criterion value, intercept excluded
+#' \item `LoFDP` LoF(DP)-criterion value
+#' \item `mseD` mse(D)-criterion value, obtained via MC sampling
+#' \item `mseP` mse(D)-criterion value, obtained using point prior 
+#' \item `Ls` Ls-criterion value, intercept excluded
+#' \item `LP` LPs-criterion value, intercept excluded
+#' \item `LoFLP` LoF(LP)-criterion value
+#' \item `mseL` mse(L)-criterion value
+#' }
 
-### Evaluation of MSE-based criteria functions
-### Not orthonormal matrices
-
-criteria.values.mse<-function(X1, X2, P, Q, Nruns, eps=10^-23, Biter=100)      # X1,X2 - not orthonormalised matrices
+criteria.values.mse<-function(search.obj, mood.obj, eps=10^-20, Biter=1000)      # X1,X2 - not orthonormalised matrices
 {
   DP<-0; LoFDP<-0; LP<-0; LoFLP<-0;
-  DF<-nlevels(as.factor(X1[,1]))
-  df<-Nruns-DF                                # df - pure error degrees of freedom
-  tau<-tau2^0.5
+  X1 <- search.obj$X1
+  X2 <- search.obj$X2
+  df<- search.obj$df                     # df - pure error degrees of freedom
+  
+  P <- mood.obj$P; Q <- mood.obj$Q
+  tau <- mood.obj$tau
+  Nruns <- mood.obj$Nruns
+  
+  alpha.DP <- mood.obj$alpha.DP
+  alpha.LP <- mood.obj$alpha.LP
+  alpha.LoF <- mood.obj$alpha.LoF
+  alpha.LoFL <- mood.obj$alpha.LoFL
+  Z0 <- mood.obj$Z0
 
-  M<-crossprod(X1[,-1])                       # information matrix of primary terms
+  M<-crossprod(X1[,-1])                  # information matrix of primary terms
   D<-prod(round(eigen(M, symmetric=TRUE, only.values=TRUE)$values,8))
   if (D>eps)
   {
@@ -20,7 +50,7 @@ criteria.values.mse<-function(X1, X2, P, Q, Nruns, eps=10^-23, Biter=100)      #
     Ds<-(D/Nruns)^(-1.0/(P-1))                        # Ds component
   } else {return (eval=0)}
 
-  Ls<-W%*%(diag(Minv)[-1])                            # Ls component
+  Ls<-W[,-1]%*%(diag(Minv)[-1])                            # Ls component
 
   M12<-crossprod(X1[,-1],X2[,-1])
   A<-Minv%*%M12
@@ -50,6 +80,12 @@ criteria.values.mse<-function(X1, X2, P, Q, Nruns, eps=10^-23, Biter=100)      #
   }
   MC<-Tvalue/Biter
   mseD<-(exp(MC)*Nruns/D)^(1./(P-1))                           # MSE(Ds)
+  
+  MM <- t(M120)%*%(Minv[-1,-1])%*%M120
+  betaP <- rep(tau, Q)                                        # prior point estimate = rep(tau,Q)
+  TvalueP <-(1+t(betaP)%*%MM%*%betaP)
+  mseP <- (Tvalue*Nruns/D)^(1./(P-1))                         # MSE(D)_s_point
 
-  list (Ds=Ds, DP=DP, LoFDP=LoFDP, mseD=mseD, Ls=Ls, LP=LP, LoFLP=LoFLP,mseL=mseL)
+  list (df = df, Ds=Ds, DP=DP, LoFDP=LoFDP, mseD=mseD, mseP=mseP, 
+        Ls=Ls, LP=LP, LoFLP=LoFLP,mseL = mseL)
 }
