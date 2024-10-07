c3ec302c995e1ef6b45f87f0d1e45652d416cc81
olga <olgaegorova11@gmail.com>
2024-10-07 16:52:12 -0500

    Adding criteria_values_G function

--- R/criteria_values_G.R
+++ R/criteria_values_G.R
@@ -1,11 +1,44 @@
+#' Evaluating individual criteria of the designs, from the Generalized compound criteria (Goos et al., 2005), (Egorova, 2017)
+#' @description Calculating values of determinant- and trace-based components of 
+#' Generalized D-, DP-, L- and LP- criteria for an output of a search object, 
+#' with model and control parameters set in a mood object.
+#' 
+#' @param search.obj Output of the `Search' function
+#' @param mood.obj Output of the `mood' function
+#' @param eps Computational tolerance, default 10^-20
+#' 
+#' @return List of the calculated values:
+#' \itemize{
+#' \item `df` pure error degrees of freedom
+#' \item `Ds` Ds-criterion value, intercept excluded
+#' \item `DP` DPs-criterion value, intercept excluded
+#' \item `LoFD` LoF(D)-criterion value from the GD-criterion
+#' \item `LoFDP` LoF(DP)-criterion value from the GDP-criterion
+#' \item `biasD` bias(D)-criterion value from the GD-criterion
+#' \item `Ls` Ls-criterion value, intercept excluded
+#' \item `LP` LPs-criterion value, intercept excluded
+#' \item `LoFL` LoF(L)-criterion value from the GL-criterion
+#' \item `LoFLP` LoF(LP)-criterion value from the GLP-criterion
+#' \item `biasL` bias(L)-criterion value from the GL-criterion
+#' }
 
-##### GD, GDP GL, GLP criteria evaluation
-
-criteria.values.G<-function(X1, X2, P, Q, Nruns, eps=10^-23)             # X1 - orthonormalised matrix
+criteria.values.G<-function(search.obj, mood.obj, eps=10^-23)             
 {
   DP<-0; LoFDP<-0; LP<-0; LoFLP<-0;
-  DF<-nlevels(as.factor(X1[,1]))              # d.f. = N-number of unique design points
-  df<-Nruns-DF                                # df - pure error degrees of freedom
+  X1 <- search.obj$X1
+  X2 <- search.obj$X2
+  df<- search.obj$df                     # df - pure error degrees of freedom
+  
+  P <- mood.obj$P; Q <- mood.obj$Q
+  tau <- mood.obj$tau; tau2 <- mood.obj$tau2
+  Nruns <- mood.obj$Nruns
+  
+  alpha.DP <- mood.obj$alpha.DP
+  alpha.LP <- mood.obj$alpha.LP
+  alpha.LoF <- mood.obj$alpha.LoF
+  alpha.LoFL <- mood.obj$alpha.LoFL
+  Z0 <- mood.obj$Z0
+  W <- mood.obj$W
 
   M<-crossprod(X1[,-1])                       # information matrix of primary terms
   D<-prod(round(eigen(M,symmetric=TRUE,only.values=TRUE)$values,8))/Nruns
@@ -17,7 +50,8 @@ criteria.values.G<-function(X1, X2, P, Q, Nruns, eps=10^-23)             # X1 - 
   {
     Ds<-D^(-1.0/(P-1))                        # Ds component
   } else {return (eval=0)}
-  Ls<-W%*%(diag(Minv)[-1])                    # Ls component
+  
+  Ls<-W[,-1]%*%(diag(Minv)[-1])               # Ls component
 
   M12<-crossprod(X1[,-1],X2[,-1])
   A<-Minv%*%M12
@@ -38,5 +72,7 @@ criteria.values.G<-function(X1, X2, P, Q, Nruns, eps=10^-23)             # X1 - 
     LoFLP<-L0.inv.trace*qf(1-alpha.LoFL, 1, df)/Q                 # LoF (LP)
   } else {DP<-0;LoFDP<0; LoFLP<-0}
 
-  list (Ds=Ds, DP=DP, LoFD=LoFD, LoFDP=LoFDP, biasD=biasD, Ls=Ls, LP=LP, LoFL=LoFL, LoFLP=LoFLP, biasL=biasL)
+  list (df = df, 
+        Ds=Ds, DP=DP, LoFD=LoFD, LoFDP=LoFDP, biasD=biasD, 
+        Ls=Ls, LP=LP, LoFL=LoFL, LoFLP=LoFLP, biasL=biasL)
 }
